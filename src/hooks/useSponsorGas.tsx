c6be3d41a039ad8418e5a8c1fb214a91ddc34a66
Karandeep Singh <karandeepsinghcaj@outlook.com>
2023-08-12 01:45:21 -0400

    updated useSponsorGas hook

--- src/hooks/useSponsorGas.tsx
+++ src/hooks/useSponsorGas.tsx
@@ -11,16 +11,17 @@ const useSponsorGas = () => {
     setIsChallengePending(true);
 
     const enc = defaultAbiCoder.encode(['address','bytes','bytes','uint','address'],[_userOperation.sender,_userOperation.initCode,_userOperation.callData,_chain,_entryPointContractAddress])
-    const id = keccak256(enc)
+    const scopeId = keccak256(enc)
     const redirect_url = `http://localhost:3001`
-
+    const paymasterId = paymaster.id
     // Step 1: Call the API
     // const response = await fetch(`http://localhost:8001/api/paymasters/0x1234?scope=1&redirect_url=http://localhost:3001`); // Use the provided URL parameter
     // const data = await response.json();
 
     // Step 2: Open the redirect URL in a new window
     // const newWindow = window.open(data.redirectUrl, '_blank');
-		const newChallengeWindow = window.open(`${paymaster.paymasterOffchainService}?scope=1&redirect_url=http://localhost:3001`,'_blank')
+    console.log(paymaster)
+		const newChallengeWindow = window.open(`${paymaster.paymasterOffchainService}?paymasterId=${paymasterId}&scope=${scopeId}&redirect_url=${redirect_url}`,'_blank')
     setChallengeWindow(newChallengeWindow);
 
     // Create a promise that resolves when data is received
